767118ce6ceca4ab6d6bf15946ef59d920c61a2b
davies-barnard <github@davies-barnard.co.uk>
2016-02-29 13:51:19 +0000

    tidying

--- greenhouse/camera.py
+++ greenhouse/camera.py
@@ -1,6 +1,7 @@
 from picamera import PiCamera
 from time import sleep
 import datetime
+import os
 
 #http://imageio.readthedocs.io/en/latest/
 try:
@@ -9,13 +10,13 @@ except ImportError:
   print("This script requires imageio")
   sys.exit()
 
-#
+#https://www.raspberrypi.org/products/sense-hat/
 try:
   from sense_hat import SenseHat
 except ImportError:
-  print("This script requires imageio")
-  sys.exit()
-  
+
+
+"""This is a picamera wrapper with helper methods for doing cool camera like things"""
 class CamWrap():
 
     camera = None
@@ -27,20 +28,33 @@ class CamWrap():
         self.camera = PiCamera()
         self.camera.rotation = 0
 
-        self.sHat = SenseHat()
-        self.sHat.set_rotation(270)
+        try:
+          self.sHat = SenseHat()
+          self.sHat.set_rotation(270)          
+        except:
+          print("No Sense Hat available")
 
+
+    ## This method takes a string and sets it as the message on the Sense Hat
     def setMessage(self,msg):
         if self.sHat is not None:
             self.sHat.show_message(msg)
+        else:
+            print(msg)
 
+
+    ## Convert the current timestamp into a datetime string
     def getTimeStamp(self):
         ds = datetime.datetime.now().strftime("%Y%m%d_%H%M")
         return ds
 
+
+    ## Stop the camera previewing.
     def stopPreview(self):
         self.camera.stop_preview()  
 
+
+    ## Take a single photo.
     def takePhoto(self,path,ext):
         try:
             self.camera.start_preview(alpha=self.ALPHA)
@@ -52,6 +66,7 @@ class CamWrap():
         finally:
             self.camera.stop_preview()
 
+    ## Record a timelapse series of images and create an animated gif.
     def recordTimeLapse(self, path, ext, length=10, delay=1):
         images = []
         try:
@@ -72,6 +87,7 @@ class CamWrap():
 
         if len(images) > 2:
             agif = self.makeGif(images, path + ".gif")
+            
 
     ## This method uses all the filenames to create a animated gif
     def makeGif(self, filenames, exportname):
@@ -85,6 +101,7 @@ class CamWrap():
         imageio.mimsave(exportname, images)
         return exportname
 
+    ## This is just a simple preview.
     def watchingYou(self):
         try:
             self.setMessage("Watching You! ;)")
@@ -94,8 +111,12 @@ class CamWrap():
         finally:
             exit()
 
-cw = CamWrap()
-ds = cw.getTimeStamp()
-cw.recordTimeLapse('./captures/' + ds,"jpg",5,1)
-#cw.takePhoto('./captures/' + ds,"jpg")
-#cw.watchingYou()
+
+"""Running in STANDALONE mode."""
+if __name__ == "__main__":
+
+    cw = CamWrap()
+    ds = cw.getTimeStamp()
+    cw.recordTimeLapse('./captures/' + ds,"jpg",5,1)
+    #cw.takePhoto('./captures/' + ds,"jpg")
+    #cw.watchingYou()
